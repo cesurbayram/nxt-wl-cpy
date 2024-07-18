94d4cc28ccd8142679e6d0d5c48513de0139e711
mstfakks <msfa.akkas@gmail.com>
2024-07-18 23:59:16 +0300

    feature: add delete operate in user

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,11 +67,18 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "spin": {
+          '0%': {transform: 'rotate(0deg)'},
+          '100%': { transform: 'rotate(360deg)' },
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "custom-spin": 'spin 1s linear infinite',
+         
       },
+      
     },
   },
   plugins: [require("tailwindcss-animate")],
