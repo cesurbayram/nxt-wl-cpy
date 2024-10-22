870ea69dd8a620a6606e9153a3d09c817a85ddb8
Cesur Bayram <cesr.bayram@gmail.com>
2024-10-22 17:31:26 +0300

    controller_pages_create

--- app/(root)/controller/page.tsx
+++ app/(root)/controller/page.tsx
@@ -1,54 +1,75 @@
 "use client"
+import React from "react";
 import ControllerList from "@/components/controller/controller-list";
+import LoadingUi from "@/components/shared/loading-ui";
 import PageWrapper from "@/components/shared/page-wrapper"
-import { Robot } from "@/types/robot.types";
+// import { Robot } from "@/types/robot.types";
 import { useRouter } from "next/navigation";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { GiRobotGrab } from "react-icons/gi";
+import { deleteController, getController } from "@/utils/service/controller";
+import { Controller } from "@/types/controller.types";
 
-export const controllers: Robot[] = [
-    {
-        id: '1',
-        intervalMs: 10,
-        ipAddress: '127.0.25.2',
-        location: 'TOYOTETSU/X550/CELL 1',
-        name: 'Controller 1',
-        maxConnection: 10,
-        model: 'YRC1000',
-        serialNumber: '287',
-        status: 'Active',
-        controllerStatus: {
-            alarm: true,
-            cycle: 'AUTO',
-            doorOpen: false,
-            error: true,
-            hold: true,
-            maintenance: 20000,
-            operating: false,
-            safeSpeed: true,
-            servo: false,
-            stop: true,
-            teach: 'PLAY'           
-        }
-    }
-]
+// export const controllers: Robot[] = [
+//     {
+//         id: '1',
+//         intervalMs: 10,
+//         ipAddress: '127.0.25.2',
+//         location: 'TOYOTETSU/X550/CELL 1',
+//         name: 'Controller 1',
+//         maxConnection: 10,
+//         model: 'YRC1000',
+//         serialNumber: '287',
+//         status: 'Active',
+//         controllerStatus: {
+//             alarm: true,
+//             cycle: 'AUTO',
+//             doorOpen: false,
+//             error: true,
+//             hold: true,
+//             maintenance: 20000,
+//             operating: false,
+//             safeSpeed: true,
+//             servo: false,
+//             stop: true,
+//             teach: 'PLAY'           
+//         }
+//     }
+// ]
 
 
 const Page = () => {
     const router = useRouter();
-    
+    const queryClient = useQueryClient();
+
+    const { data: controller, isLoading } = useQuery<Controller[], string>({
+        queryFn: () => getController(),
+        queryKey: ["controller"]
+    });
+
+    const { mutateAsync: deleteMutation, isPending } = useMutation({
+        mutationFn: ({id}: Controller) => deleteController({id}),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["controller"]})
+        }
+    })
+
     const handleRouteControllerCreate = () => {
         router.push('/controller/0')
     }
     
     return(
+        <>
+        <LoadingUi isLoading={isLoading || isPending} />
         <PageWrapper
             buttonText="Add New Controller"
             pageTitle="Controllers"
             icon={<GiRobotGrab size={24} color="#6950e8" />}
             buttonAction={handleRouteControllerCreate}
         >
-            <ControllerList controllers={controllers || []} />
+            <ControllerList controller={controller || []} deleteClick={deleteMutation}/>
         </PageWrapper>
+        </>
     )
 }
 
--- app/api/controller/route.ts
+++ app/api/controller/route.ts
@@ -1,11 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { dbPool } from '@/utils/dbUtil';
 import { v4 as uuidv4 } from 'uuid';
-import { Robot } from '@/types/robot.types';
+//import { Robot } from '@/types/robot.types';
+//import { Controller } from '@/types/controller.types';
+
+export interface Controller {
+    id: string;
+    name: string;
+    model: string;
+    ipAddress: string;
+    status: string;
+    serialNumber: string;
+    intervalMs: number;
+    maxConnection: number;
+    location: string;
+    createdAt: string;
+    updatedAt: string;
+    deletedAt: string; 
+}
 
 export async function GET(request: NextRequest) {
     try {
-        const robotDbResp = await dbPool.query(`
+        const controllerDbResp = await dbPool.query(`
             SELECT 
                 r.id,
                 r.name,
@@ -19,10 +35,11 @@ export async function GET(request: NextRequest) {
                 r.created_at AS "createdAt",
                 r.updated_at AS "updatedAt"
             FROM 
-                "robot" r
-        `);                
-        const robots: Robot[] = robotDbResp.rows;
-        return NextResponse.json(robots, { status: 200 });
+                "controller" r
+        `); 
+
+        const controllers: Controller[] = controllerDbResp.rows;
+        return NextResponse.json(controllers, { status: 200 });
     } catch (error: any) {
         console.error('DB ERROR:', error.message);
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
@@ -30,28 +47,28 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-    const { name, model, ipAddress, status, serialNumber, intervalMs, maxConnection, location }: Robot = await request.json();
+    const { name, model, ipAddress, status, serialNumber, intervalMs, maxConnection, location }: Controller = await request.json();
     const client = await dbPool.connect();
     const newRobotId = uuidv4();
     const newRobotStatusId = uuidv4();
     try {
         await client.query('BEGIN');
         await client.query(
-            `INSERT INTO "robot" (id, name, model, ip_address, status, serial_number, interval_ms, max_connection, location)
+            `INSERT INTO "controller" (id, name, model, ip_address, status, serial_number, interval_ms, max_connection, location)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
             [newRobotId, name, model, ipAddress, status, serialNumber, intervalMs, maxConnection, location]
         );
 
-        await client.query(`INSERT INTO robot_status (id, ip_address, teach, servo, operating, cycle, hold, alarm, error, stop) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, [
-            newRobotStatusId, ipAddress, 'TEACH', false, false, 'CYCLE', false, false, false, false
-        ]);
+        // await client.query(`INSERT INTO controller_status (id, ip_address, teach, servo, operating, cycle, hold, alarm, error, stop) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, [
+        //     newRobotStatusId, ipAddress, 'TEACH', false, false, 'CYCLE', false, false, false, false
+        // ]);
 
-        await client.query(
-            `INSERT INTO d_read (id, ip_address, no, name, value)
-             SELECT uuid_generate_v4(), $1, gs, null, '0'
-             FROM generate_series(0, 3000) as gs;`,
-            [ipAddress]
-        );
+        // await client.query(
+        //     `INSERT INTO d_read (id, ip_address, no, name, value)
+        //      SELECT uuid_generate_v4(), $1, gs, null, '0'
+        //      FROM generate_series(0, 3000) as gs;`,
+        //     [ipAddress]
+        // );
 
         // await client.query(
         //     `INSERT INTO s_read (id, ip_address, no, name, value)
@@ -75,7 +92,7 @@ export async function POST(request: NextRequest) {
         // );
         
         await client.query('COMMIT');
-        return NextResponse.json({ message: 'Robot created successfully' }, { status: 201 });
+        return NextResponse.json({ message: 'Controller created successfully' }, { status: 201 });
     } catch (error: any) {
         console.error('DB ERROR:', error.message);
         await client.query('ROLLBACK');
@@ -86,18 +103,18 @@ export async function POST(request: NextRequest) {
 }
 
 export async function PUT(request: NextRequest) {
-    const { id, name, model, ipAddress, status, serialNumber, intervalMs, maxConnection, location }: Robot = await request.json();
+    const { id, name, model, ipAddress, status, serialNumber, intervalMs, maxConnection, location }: Controller = await request.json();
     const client = await dbPool.connect();
     try {
         await client.query('BEGIN');
         await client.query(
-            `UPDATE "robot" 
-            SET name = $1, model = $2, ip_address = $3, status = $4, serial_number = $5, interval_ms = $6, max_connection = $7, location = $8, update_at = now() 
+            `UPDATE "controller" 
+            SET name = $1, model = $2, ip_address = $3, status = $4, serial_number = $5, interval_ms = $6, max_connection = $7, location = $8 
             WHERE id = $9`,
             [name, model, ipAddress, status, serialNumber, intervalMs, maxConnection, location, id]
         );
         await client.query('COMMIT');
-        return NextResponse.json({ message: 'Robot updated successfully' }, { status: 200 });
+        return NextResponse.json({ message: 'Controller updated successfully' }, { status: 200 });
     } catch (error: any) {
         console.error('DB ERROR:', error.message);
         await client.query('ROLLBACK');
@@ -108,13 +125,13 @@ export async function PUT(request: NextRequest) {
 }
 
 export async function DELETE(request: NextRequest) {
-    const { id }: { id: string } = await request.json();
+    const { id } = await request.json();
     const client = await dbPool.connect();
     try {
         await client.query('BEGIN');
-        await client.query(`DELETE FROM "robot" WHERE id = $1`, [id]);
+        await client.query(`DELETE FROM "controller" WHERE id = $1`, [id]);
         await client.query('COMMIT');
-        return NextResponse.json({ message: 'Robot deleted successfully' }, { status: 200 });
+        return NextResponse.json({ message: 'Controller deleted successfully' }, { status: 200 });
     } catch (error: any) {
         console.error('DB ERROR:', error.message);
         await client.query('ROLLBACK');
