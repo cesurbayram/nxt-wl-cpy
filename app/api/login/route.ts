8471ded48292673a8cf40a9ec066edc7900777e0
mstfakks <msfa.akkas@gmail.com>
2024-10-23 01:02:05 +0300

    feature: implement login functionality

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,6 +3,7 @@ import { User } from "@/types/user.types";
 import { dbPool } from "@/utils/dbUtil";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export async function POST(request: NextRequest) {
   const { email, password }: Login = await request.json();
@@ -33,8 +34,30 @@ export async function POST(request: NextRequest) {
     const userData: User = userDbRes.rows[0];
     const isPasswordMatch = await bcrypt.compare(password, userData.bcryptPassword ? userData.bcryptPassword : '');
     
-    console.log('isPasswordMatch', isPasswordMatch);
-    return NextResponse.json({message: 'success'}, {status:200})
+    if(!isPasswordMatch){
+        return NextResponse.json({error: "Wrong password!"}, {status: 400})
+    }
+
+    const tokenData = {
+        name: userData.name,
+        lastName: userData.lastName,
+        userName: userData.userName,
+        email: userData.email,
+        role: userData.role        
+    }
+
+    const token = await jwt.sign(tokenData, process.env.SECRET, {
+        expiresIn: "24h"  
+    })
+
+    const response = NextResponse.json({
+        message: "Successfully logged in",
+        success: true
+    })
+
+    response.cookies.set("token", token, {httpOnly: true})
+
+    return response
   } catch (error: any) {
     return NextResponse.json({error: error?.message}, {status: 500})
   }
---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export function middleware(request: NextRequest){
+    
+    const currentPath = request.nextUrl.pathname
+    const isPublic = currentPath === "/sign-in"
+    const token = request.cookies.get("token")?.value || ''
+    if(isPublic && token.length > 0){
+        console.log('burada');
+        
+        return NextResponse.redirect(new URL("/", request.nextUrl))
+    }
+    if(!isPublic && !(token.length > 0)){
+        return NextResponse.redirect(new URL("/sign-in", request.nextUrl))
+    }
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
+}
