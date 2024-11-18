b51f6edde4f8d9e6cf40f663f81fb84aa774a7d7
mstfakks <msfa.akkas@gmail.com>
2024-11-19 00:21:24 +0300

    feature: implement status bar for controller

--- app/api/controller/[id]/route.ts
+++ app/api/controller/[id]/route.ts
@@ -1,28 +1,51 @@
 import { dbPool } from "@/utils/dbUtil";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const dbRes = await dbPool.query(`SELECT
-            id,
-            name,
-            model,
-            application,
-            ip_address AS "ipAddress",
-            status,
-            serial_number AS "serialNumber",
-            interval_ms AS "intervalMs",
-            max_connection AS "maxConnection",
-            location            
-            FROM controller WHERE id = $1`, [params.id]);
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const dbRes = await dbPool.query(
+      `SELECT
+            c.id,
+            c.name,
+            c.model,
+            c.application,
+            c.ip_address AS "ipAddress",
+            c.status,
+            c.serial_number AS "serialNumber",
+            c.interval_ms AS "intervalMs",
+            c.max_connection AS "maxConnection",
+            c.location,
+            json_build_object(
+        'alarm', ct.alarm,
+        'cycle', ct.cycle,
+        'doorOpen', ct.door_opened,
+        'error', ct.error,
+        'hold', ct.hold,
+        'operating', ct.operating,
+        'servo', ct.servo,
+        'stop', ct.stop,
+        'teach', ct.teach
+    ) AS "controllerStatus"           
+            FROM controller c INNER JOIN controller_status ct ON c.id=ct.controller_id WHERE c.id = $1`,
+      [params.id]
+    );
 
-        if (dbRes?.rowCount && dbRes.rowCount > 0) {
-            const controller = dbRes.rows[0];
-            return NextResponse.json({ ...controller });
-        }
-        return NextResponse.json({ message: 'Controller not found' }, { status: 404 });
-    } catch (error: any) {
-        console.log('DB Error: ', error.message);
-        return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+    if (dbRes?.rowCount && dbRes.rowCount > 0) {
+      const controller = dbRes.rows[0];
+      return NextResponse.json({ ...controller });
     }
+    return NextResponse.json(
+      { message: "Controller not found" },
+      { status: 404 }
+    );
+  } catch (error: any) {
+    console.log("DB Error: ", error.message);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
--- app/api/controller/route.ts
+++ app/api/controller/route.ts
@@ -1,127 +1,229 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { dbPool } from '@/utils/dbUtil';
-import { v4 as uuidv4 } from 'uuid';
+import { NextRequest, NextResponse } from "next/server";
+import { dbPool } from "@/utils/dbUtil";
+import { v4 as uuidv4 } from "uuid";
 //import { Robot } from '@/types/robot.types';
 //import { Controller } from '@/types/controller.types';
 
 export interface Controller {
-    id: string;
-    name: string;
-    model: string;
-    application: string;
-    ipAddress: string;
-    status: string;
-    serialNumber: string;
-    intervalMs: number;
-    maxConnection: number;
-    location: string;
-    createdAt: string;
-    updatedAt: string;
-    deletedAt: string; 
+  id: string;
+  name: string;
+  model: string;
+  application: string;
+  ipAddress: string;
+  status: string;
+  serialNumber: string;
+  intervalMs: number;
+  maxConnection: number;
+  location: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
 }
 
 export async function GET(request: NextRequest) {
-    try {
-        const controllerDbResp = await dbPool.query(`
-            SELECT 
-                r.id,
-                r.name,
-                r.model,
-                r.application,
-                r.ip_address AS "ipAddress",
-                r.status,
-                r.serial_number AS "serialNumber",
-                r.interval_ms AS "intervalMs",
-                r.max_connection AS "maxConnection",
-                r.location,
-                r.created_at AS "createdAt",
-                r.updated_at AS "updatedAt"
-            FROM 
-                "controller" r
-        `); 
+  try {
+    // const controllerDbResp = await dbPool.query(`
+    //     SELECT
+    //         r.id,
+    //         r.name,
+    //         r.model,
+    //         r.application,
+    //         r.ip_address AS "ipAddress",
+    //         r.status,
+    //         r.serial_number AS "serialNumber",
+    //         r.interval_ms AS "intervalMs",
+    //         r.max_connection AS "maxConnection",
+    //         r.location,
+    //         r.created_at AS "createdAt",
+    //         r.updated_at AS "updatedAt"
+    //     FROM
+    //         "controller" r
+    // `);
 
-        const controllers: Controller[] = controllerDbResp.rows;
-        return NextResponse.json(controllers, { status: 200 });
-    } catch (error: any) {
-        console.error('DB ERROR:', error.message);
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    }
+    const controllerDbResp = await dbPool.query(`
+            SELECT
+    c.id,
+    c.ip_address AS "ipAddress",
+    c.name,
+    c.model,
+    c.application,
+    c.status,
+    json_build_object(
+        'alarm', ct.alarm,
+        'cycle', ct.cycle,
+        'doorOpen', ct.door_opened,
+        'error', ct.error,
+        'hold', ct.hold,
+        'operating', ct.operating,
+        'servo', ct.servo,
+        'stop', ct.stop,
+        'teach', ct.teach
+    ) AS "controllerStatus"
+    FROM
+    controller c
+        INNER JOIN controller_status ct
+            ON c.id = ct.controller_id
+        `);
+
+    const controllers: Controller[] = controllerDbResp.rows;
+    return NextResponse.json(controllers, { status: 200 });
+  } catch (error: any) {
+    console.error("DB ERROR:", error.message);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(request: NextRequest) {
-    const { name, model, application, ipAddress, status, serialNumber, intervalMs, maxConnection, location }: Controller = await request.json();
-    const client = await dbPool.connect();
-    const newRobotId = uuidv4();
-    const newRobotStatusId = uuidv4();
-    try {
-        await client.query('BEGIN');
-        await client.query(
-            `INSERT INTO "controller" (id, name, model, application, ip_address, status, serial_number, interval_ms, max_connection, location)
+  const {
+    name,
+    model,
+    application,
+    ipAddress,
+    status,
+    serialNumber,
+    intervalMs,
+    maxConnection,
+    location,
+  }: Controller = await request.json();
+  const client = await dbPool.connect();
+  const newRobotId = uuidv4();
+  const newRobotStatusId = uuidv4();
+  try {
+    await client.query("BEGIN");
+    await client.query(
+      `INSERT INTO "controller" (id, name, model, application, ip_address, status, serial_number, interval_ms, max_connection, location)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-            [newRobotId, name, model, application, ipAddress, status, serialNumber, intervalMs, maxConnection, location]
-        );
+      [
+        newRobotId,
+        name,
+        model,
+        application,
+        ipAddress,
+        status,
+        serialNumber,
+        intervalMs,
+        maxConnection,
+        location,
+      ]
+    );
 
+    await client.query(
+      `INSERT INTO controller_status (id, ip_address, controller_id, teach, servo, operating, cycle,  hold, alarm, error, stop, door_opened ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+      [
+        newRobotStatusId,
+        ipAddress,
+        newRobotId,
+        "TEACH",
+        false,
+        false,
+        "CYCLE",
+        false,
+        false,
+        false,
+        false,
+        false,
+      ]
+    );
+
+    const tables = ["b_read", "d_read", "s_read", "i_read", "r_read"];
+    for (const table of tables) {
+      for (let i = 0; i < 11; i++) {
         await client.query(
-            `INSERT INTO controller_status (id, ip_address, controller_id, teach, servo, operating, cycle,  hold, alarm, error, stop, door_opened ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`, 
-            [newRobotStatusId, ipAddress, newRobotId, 'TEACH', false, false, 'CYCLE', false, false, false, false, false]
+          `INSERT INTO ${table} (id, ip_address, no, name, value, controller_id) VALUES ($1, $2, $3, $4, $5, $6)`,
+          [uuidv4(), ipAddress, i, null, "0", newRobotId]
         );
-
-        const tables = ['b_read', 'd_read', 's_read', 'i_read', 'r_read'];
-        for (const table of tables) {
-            for (let i = 0; i < 11; i++) {
-                await client.query(
-                    `INSERT INTO ${table} (id, ip_address, no, name, value, controller_id) VALUES ($1, $2, $3, $4, $5, $6)`,
-                    [uuidv4(), ipAddress, i, null, '0', newRobotId]
-                );
-            }
-        }
-
-        await client.query('COMMIT');
-        return NextResponse.json({ message: 'Controller created successfully' }, { status: 201 });
-    } catch (error: any) {
-        console.error('DB ERROR:', error.message);
-        await client.query('ROLLBACK');
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    } finally {
-        client.release();
+      }
     }
+
+    await client.query("COMMIT");
+    return NextResponse.json(
+      { message: "Controller created successfully" },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    console.error("DB ERROR:", error.message);
+    await client.query("ROLLBACK");
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  } finally {
+    client.release();
+  }
 }
 
 export async function PUT(request: NextRequest) {
-    const { id, name, model, application, ipAddress, status, serialNumber, intervalMs, maxConnection, location }: Controller = await request.json();
-    const client = await dbPool.connect();
-    try {
-        await client.query('BEGIN');
-        await client.query(
-            `UPDATE "controller" 
+  const {
+    id,
+    name,
+    model,
+    application,
+    ipAddress,
+    status,
+    serialNumber,
+    intervalMs,
+    maxConnection,
+    location,
+  }: Controller = await request.json();
+  const client = await dbPool.connect();
+  try {
+    await client.query("BEGIN");
+    await client.query(
+      `UPDATE "controller" 
             SET name = $1, model = $2, application = $3, ip_address = $4, status = $5, serial_number = $6, interval_ms = $7, max_connection = $8, location = $9 
             WHERE id = $10`,
-            [name, model, application, ipAddress, status, serialNumber, intervalMs, maxConnection, location, id]
-        );
-        await client.query('COMMIT');
-        return NextResponse.json({ message: 'Controller updated successfully' }, { status: 200 });
-    } catch (error: any) {
-        console.error('DB ERROR:', error.message);
-        await client.query('ROLLBACK');
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    } finally {
-        client.release();
-    }
+      [
+        name,
+        model,
+        application,
+        ipAddress,
+        status,
+        serialNumber,
+        intervalMs,
+        maxConnection,
+        location,
+        id,
+      ]
+    );
+    await client.query("COMMIT");
+    return NextResponse.json(
+      { message: "Controller updated successfully" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("DB ERROR:", error.message);
+    await client.query("ROLLBACK");
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  } finally {
+    client.release();
+  }
 }
 
 export async function DELETE(request: NextRequest) {
-    const { id } = await request.json();
-    const client = await dbPool.connect();
-    try {
-        await client.query('BEGIN');
-        await client.query(`DELETE FROM "controller" WHERE id = $1`, [id]);
-        await client.query('COMMIT');
-        return NextResponse.json({ message: 'Controller deleted successfully' }, { status: 200 });
-    } catch (error: any) {
-        console.error('DB ERROR:', error.message);
-        await client.query('ROLLBACK');
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    } finally {
-        client.release();
-    }
+  const { id } = await request.json();
+  const client = await dbPool.connect();
+  try {
+    await client.query("BEGIN");
+    await client.query(`DELETE FROM "controller" WHERE id = $1`, [id]);
+    await client.query("COMMIT");
+    return NextResponse.json(
+      { message: "Controller deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("DB ERROR:", error.message);
+    await client.query("ROLLBACK");
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  } finally {
+    client.release();
+  }
 }
