1e5f8634f2c132f0131f26853b9db537e9002fa9
Cesur Bayram <cesr.bayram@gmail.com>
2025-01-29 17:52:51 +0300

    timer changed

--- components/shared/timer.tsx
+++ components/shared/timer.tsx
@@ -1,47 +1,51 @@
 import { useEffect, useState } from "react";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const Timer = ({ callback }: { callback: any }) => {
-    const [selectedVal, setSelectedVal] = useState<string>("false");
+  const [selectedVal, setSelectedVal] = useState<string>("false");
 
-    const handleChange = (value: string) => {
-        setSelectedVal(value)
+  const handleChange = (value: string) => {
+    setSelectedVal(value);
+  };
+
+  console.log("selectedVal", selectedVal);
+
+  useEffect(() => {
+    if (selectedVal !== "false") {
+      const interval = setInterval(() => callback(), Number(selectedVal));
+      return () => {
+        clearInterval(interval);
+      };
     }
+  }, [selectedVal]);
 
-    console.log('selectedVal', selectedVal);
-
-    useEffect(() => {
-        if(selectedVal !== "false"){
-            const interval = setInterval(() => callback(), Number(selectedVal))
-            return () => {
-                clearInterval(interval)
-            }
-        }
-    }, [selectedVal])
-    
-    return(
-        <div className="flex items-center gap-2">
-            <label>Timer</label>
-            <Select
-                value={selectedVal}
-                onValueChange={handleChange}            
-            >
-                <SelectTrigger>
-                    <SelectValue placeholder="Activate Timer" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>Times</SelectLabel>
-                        <SelectItem value="false">Disable</SelectItem>
-                        <SelectItem value={"1000"}>1000ms</SelectItem>
-                        <SelectItem value={"2000"}>2000ms</SelectItem>
-                        <SelectItem value={"3000"}>3000ms</SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-        </div>   
-    )
-}
-
+  return (
+    <div className="flex items-center gap-2">
+      <label>Timer</label>
+      <Select value={selectedVal} onValueChange={handleChange}>
+        <SelectTrigger>
+          <SelectValue placeholder="Activate Timer" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Times</SelectLabel>
+            <SelectItem value="false">Disable</SelectItem>
+            <SelectItem value={"100"}>100ms</SelectItem>
+            <SelectItem value={"200"}>200ms</SelectItem>
+            <SelectItem value={"300"}>1000ms</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
 
 export default Timer;
