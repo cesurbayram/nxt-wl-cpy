3f91a5f70556f29db689275fb8075f9cbe01a5db
Cesur Bayram <cesr.bayram@gmail.com>
2025-05-30 01:05:01 +0300

    changed amlog

--- components/sign-in/hero.tsx
+++ components/sign-in/hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center">
       <p className="text-white text-4xl">Hi, Welcome Back!</p>
-      <p className="text-4xl text-white font-bold mt-4">amLog</p>
+      <p className="text-4xl text-white font-bold mt-4">WatchLog</p>
     </div>
   );
 };
