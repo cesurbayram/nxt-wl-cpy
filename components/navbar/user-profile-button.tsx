e4c51854653dc0291658934b32703521aeee068a
Cesur Bayram <cesr.bayram@gmail.com>
2025-06-02 12:14:55 +0300

    changed login and logout

--- components/navbar/user-profile-button.tsx
+++ components/navbar/user-profile-button.tsx
@@ -1,41 +1,64 @@
-"use client"
+"use client";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { CgProfile } from "react-icons/cg";
-import { userLogout } from "@/utils/service/auth";
+import { userLogout, getUserAfterAuth } from "@/utils/service/auth";
 import { useRouter } from "next/navigation";
-import { deleteDataFromStorage, getDataFromStorage } from "@/utils/common/storage";
+import {
+  deleteDataFromStorage,
+  getDataFromStorage,
+  addStorage,
+} from "@/utils/common/storage";
+import { useEffect, useState } from "react";
+import { User } from "@/types/user.types";
 
 const formatName = (fullName: string) => {
-  let result = '';
-  const splittedName = fullName.split(' ');
+  let result = "";
+  const splittedName = fullName.split(" ");
   splittedName.forEach((item) => {
-    if(item.length > 0) {
-      result += item.substring(0,1).toUpperCase();
+    if (item.length > 0) {
+      result += item.substring(0, 1).toUpperCase();
     }
-  })
+  });
 
   return result;
-}
+};
 
 const UserProfileButton = () => {
-  const router = useRouter()
-  const user = getDataFromStorage('user');  
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      let userData = getDataFromStorage("user");
+
+      if (!userData) {
+        try {
+          userData = await getUserAfterAuth();
+          addStorage("user", userData);
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      }
+
+      setUser(userData);
+    };
+
+    loadUser();
+  }, []);
 
   const logout = async () => {
     try {
       await userLogout();
-      deleteDataFromStorage('user');
-      router.push('/sign-in');
+      deleteDataFromStorage("user");
+      router.push("/sign-in");
     } catch (error) {
-      console.error('An error occured while logout: ', error)
+      console.error("An error occured while logout: ", error);
     }
-  }
-
-
+  };
 
   return (
-    <Popover>      
+    <Popover>
       <PopoverTrigger asChild>
         <Button variant={"ghost"} size={"icon"} className="p-2 rounded-full">
           <CgProfile size={24} />
@@ -46,18 +69,28 @@ const UserProfileButton = () => {
         <div className="grid grid-cols-1 gap-y-4 ">
           <div className="flex items-center gap-3">
             <div className="w-10 h-10 rounded-full bg-gray-300 flex justify-center items-center border-2">
-              <p className="font-semibold">{formatName(`${user?.name || ''} ${user?.lastName || ''}`)}</p>
+              <p className="font-semibold">
+                {formatName(`${user?.name || ""} ${user?.lastName || ""}`)}
+              </p>
             </div>
             <div>
-              <p className="font-semibold text-sm">{user?.name || ''} {user?.lastName || ''}</p>
-              <p className="font-normal text-xs text-[#6b7280]">{user?.email || ''}</p>
-              <p className="font-normal text-xs text-[#6b7280]">{user?.role || ''}</p>
+              <p className="font-semibold text-sm">
+                {user?.name || ""} {user?.lastName || ""}
+              </p>
+              <p className="font-normal text-xs text-[#6b7280]">
+                {user?.email || ""}
+              </p>
+              <p className="font-normal text-xs text-[#6b7280]">
+                {user?.role || ""}
+              </p>
             </div>
           </div>
 
           <div className="border border-black border-opacity-20" />
-          
-          <Button onClick={logout} variant={'ghost'} size={'sm'}>Sign Out</Button>
+
+          <Button onClick={logout} variant={"ghost"} size={"sm"}>
+            Sign Out
+          </Button>
         </div>
       </PopoverContent>
     </Popover>
