d751757e23d650fce4c926c4d0696f5af3b1d27c
Cesur Bayram <cesr.bayram@gmail.com>
2025-06-23 01:09:42 +0300

    added new fonctioun force-dynamic

--- app/api/controller/[id]/utilization/route.ts
+++ app/api/controller/[id]/utilization/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { dbPool } from "@/utils/dbUtil";
 import { v4 as uuidv4 } from "uuid";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
--- app/api/shift/production-value/compare/route.ts
+++ app/api/shift/production-value/compare/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { dbPool } from "@/utils/dbUtil";
 import { ProductionComparison } from "@/types/job-status.types";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
