fbb0be402338096c85b3ec6465206d7581dbd7e4
Cesur Bayram <cesr.bayram@gmail.com>
2025-07-31 10:42:53 +0300

    changed url

--- utils/service/files/index.ts
+++ utils/service/files/index.ts
@@ -74,16 +74,19 @@ const manualBackup = async (
   error?: string;
 }> => {
   try {
-    const response = await fetch("http://localhost:8082/api/manual-backup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        controllerId,
-        fileTypes,
-      }),
-    });
+    const response = await fetch(
+      "https://savola-senddata.fabricademo.com/api/manual-backup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          controllerId,
+          fileTypes,
+        }),
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Failed to initiate backup");
